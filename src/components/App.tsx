78749e73728402c8a0eaaa0f88f013ef293506a3
Syed Taqi Haider <sh13226@my.bristol.ac.uk>
2021-10-09 11:02:33 +0100

    Added plotly.js and added a simple plot

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,25 @@
 import React from "react";
+import Plotly from "plotly.js-basic-dist";
+import createPlotlyComponent from "react-plotly.js/factory";
 
 function App() {
+  const Plot = createPlotlyComponent(Plotly);
   return (
     <div>
-      <h1>Hello world</h1>
+      <h1>UK Business Activity Visualiser</h1>
+      <Plot
+        data={[
+          {
+            x: [1, 2, 3],
+            y: [2, 6, 3],
+            type: "scatter",
+            mode: "lines+markers",
+            marker: { color: "red" },
+          },
+          { type: "bar", x: [1, 2, 3], y: [2, 5, 3] },
+        ]}
+        layout={{ width: 320, height: 240, title: "A Fancy Plot" }}
+      />
     </div>
   );
 }
