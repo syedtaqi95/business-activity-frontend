8a6d0a483faa1db288ad670bfe4c714acddbbc90
Syed Taqi Haider <sh13226@my.bristol.ac.uk>
2021-10-10 13:19:56 +0100

    Added Footer component

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      Developed by Joel Edwardson and Syed Taqi Haider, 2021. View the source on{" "}
+      <a href="https://github.com/syedtaqi95/business-activity-frontend">
+        Github
+      </a>
+      .
+    </div>
+  );
+};
+
+export default Footer;
