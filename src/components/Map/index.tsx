e8d048dcc534c2e3812e82bfed00edd26cf34aaf
Syed Taqi Haider <sh13226@my.bristol.ac.uk>
2021-10-10 12:48:05 +0100

    Added a popup to the Map

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -60,10 +60,16 @@ const Map = () => {
         },
       });
 
-      // When the user moves their mouse over an area, we'll update the
-      // feature state for the feature under the mouse.
+      // Create a popup but don't add it to the map yet.
+      const popup = new mapboxgl.Popup({
+        closeButton: false,
+        closeOnClick: false,
+      });
+
       map.on("mousemove", "countries-layer", (e) => {
         if (e.features.length > 0) {
+          // When the user moves their mouse over an area, we'll update the
+          // feature state for the feature under the mouse.
           if (hoveredAreaRef.current && hoveredAreaRef.current > -1) {
             map.setFeatureState(
               { source: "countries-source", id: hoveredAreaRef.current },
@@ -78,12 +84,26 @@ const Map = () => {
           );
 
           setHoveredArea(_hoveredArea);
+
+          // Change the cursor style as a UI indicator.
+          map.getCanvas().style.cursor = "pointer";
+
+          // Get the pointer coordinates
+          const coordinates: mapboxgl.LngLat = e.lngLat;
+          const popupData = `
+            <strong>${e.features[0].properties.CTRY20NM}</strong>
+            <div>Business enterprises: 4,200,000</div>
+            `;
+
+          // Populate the popup and set its coordinates
+          // based on the feature found.
+          popup.setLngLat(coordinates).setHTML(popupData).addTo(map);
         }
       });
 
-      // When the mouse leaves the state-fill layer, update the feature state of the
-      // previously hovered feature.
-      map.on("mouseleave", "countries-layer", function () {
+      map.on("mouseleave", "countries-layer", () => {
+        // When the mouse leaves the state-fill layer, update the feature state of the
+        // previously hovered feature.
         if (hoveredAreaRef.current) {
           map.setFeatureState(
             { source: "countries-source", id: hoveredAreaRef.current },
@@ -91,8 +111,13 @@ const Map = () => {
           );
         }
         setHoveredArea(null);
+
+        // Remove the popup and reset the cursor
+        map.getCanvas().style.cursor = '';
+        popup.remove();
       });
 
+      // Update the lat, lng and zoom states when the map moves
       map.on("move", () => {
         setLng(Number(map.getCenter().lng.toFixed(4)));
         setLat(Number(map.getCenter().lat.toFixed(4)));
