f2485eecb3558394afed96d836fc1cd6aaebefe5
Syed Taqi Haider <sh13226@my.bristol.ac.uk>
2021-10-23 16:25:01 +0100

    Github link opens in a new tab

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -5,7 +5,11 @@ const Footer = () => {
   return (
     <div className="footer">
       Developed by Joel Edwardson and Syed Taqi Haider, 2021. View the source on{" "}
-      <a href="https://github.com/syedtaqi95/business-activity-frontend">
+      <a
+        target="_blank"
+        rel="noreferrer"
+        href="https://github.com/syedtaqi95/business-activity-frontend"
+      >
         Github
       </a>
       .
