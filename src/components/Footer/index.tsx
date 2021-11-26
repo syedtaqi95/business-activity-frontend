fa59888fd510e8970912b2c50072f15a5680c179
joel.edwardson1@gmail.com <joel.edwardson1@gmail.com>
2021-11-26 11:24:02 +0000

    updated footer and removed engine requirements

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,15 +4,15 @@ import "./Footer.css";
 const Footer = () => {
   return (
     <div className="footer">
-      Developed by Joel Edwardson and Syed Taqi Haider, 2021. View the source on{" "}
-      <a
+      Developed by Joel Edwardson and Syed Taqi Haider, 2021. View the <a
         target="_blank"
         rel="noreferrer"
         href="https://github.com/syedtaqi95/business-activity-frontend"
-      >
-        Github
-      </a>
-      .
+      >frontend source</a> and <a
+      target="_blank"
+      rel="noreferrer"
+      href="https://github.com/joeledwardson/business-activity-visualiser"
+      >backend source</a>.
     </div>
   );
 };
